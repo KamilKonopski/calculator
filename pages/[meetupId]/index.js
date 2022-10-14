a39ce54f9124cfac7b9aae4b38ad173732489d01
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2022-10-14 21:10:29 +0200

    add meetup detail component

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+	return (
+		<MeetupDetail
+			image="https://upload.wikimedia.org/wikipedia/commons/0/05/PKiN_widziany_z_WFC.jpg"
+			title="First Meetup"
+			address="street 8, Warsaw"
+			description="This is a first meetup"
+		/>
+	);
+}
+
+export default MeetupDetails;
