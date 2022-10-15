3f5d92273db8688b10a2dd247f02a6792d8a047c
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2022-10-15 21:45:45 +0200

    add getStaticProps function

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,23 @@ function MeetupDetails() {
 	);
 }
 
+export async function getStaticProps(context) {
+	// fetch data for a single meetup
+
+	const meetupId = context.params.meetupId;
+
+	return {
+		props: {
+			meetupData: {
+				id: meetupId,
+				image:
+					"https://upload.wikimedia.org/wikipedia/commons/0/05/PKiN_widziany_z_WFC.jpg",
+				title: "First Meetup",
+				address: "street 8, Warsaw",
+				description: "This is a first meetup",
+			},
+		},
+	};
+}
+
 export default MeetupDetails;
