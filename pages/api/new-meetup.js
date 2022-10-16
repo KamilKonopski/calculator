ce1d9d54af6da6dfb3a809263ab48f81cb30013a
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2022-10-16 19:29:04 +0200

    connect with database

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,8 +1,21 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
 	if (req.method === "POST") {
 		const data = req.body;
 
-		const { title, image, address, description } = data;
+		const client = await MongoClient.connect(
+			"mongodb+srv://MaerS:<password>@cluster0.jhdjp2o.mongodb.net/?retryWrites=true&w=majority"
+		);
+		const db = client.db();
+
+		const meetupsCollection = db.collection("meetups");
+
+		const result = await meetupsCollection.insertOne(data);
+
+		client.close();
+
+		res;
 	}
 }
 
