685d64c3f57a48fd542773adaa973fedd4f6fd0b
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2024-04-15 15:55:22 +0200

    add stats state and add to jsx

--- src/components/Statistic/Statistic.tsx
+++ src/components/Statistic/Statistic.tsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 import classes from "./Statistic.module.scss";
 
 const Statistic: React.FC = () => {
+	const [stats, setStats] = useState<{ wins: number; losses: number }>({
+		wins: 0,
+		losses: 0
+	})
+
 	return (
 		<div className={classes.stats}>
 			<span className={classes["stats__text"]}>
-				Number of wins: <span className={classes["stats__wins"]}>0</span>
+				Number of wins: <span className={classes["stats__wins"]}>{stats.wins}</span>
 			</span>
 			<span className={classes["stats__text"]}>
-				Number of losses: <span className={classes["stats__losses"]}>0</span>
+				Number of losses: <span className={classes["stats__losses"]}>{stats.losses}</span>
 			</span>
 		</div>
 	);
