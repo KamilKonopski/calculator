2a6fcd9fceb3542a853f1178a101b7d669af2872
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2024-04-16 16:26:54 +0200

    add statistic component & add stats state

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 import StartGame from "./components/StartGame/StartGame";
 import Game from "./components/Game/Game";
+import Statistic from "./components/Statistic/Statistic";
 
 import classes from "./App.module.scss";
 
@@ -17,14 +18,20 @@ const DUMMY_QUIZ = {
 };
 
 const App: React.FC = () => {
-	const [isInGame, setIsInGame] = useState(false);
+	const [isInGame, setIsInGame] = useState<boolean>(false);
+	const [stats, setStats] = useState<{ wins: number; losses: number }>({
+		wins: 0,
+		losses: 0
+	})
 
+	
 	return (
 		<>
 			<h1 className={classes.heading}>Countries Quiz</h1>
 			<div className={classes.app}>
 				{!isInGame && <StartGame startGame={() => setIsInGame(true)} />}
-				{isInGame && <Game quizData={DUMMY_QUIZ} />}
+				{!isInGame && <Statistic stats={stats}/>}
+				{isInGame && <Game quizData={DUMMY_QUIZ} quizWin={() => setStats({...stats, wins: stats.wins + 1})} quizLose={() => setStats({...stats, losses: stats.losses + 1})} gameOver={() => setIsInGame(false)}/>}
 			</div>
 		</>
 	);
