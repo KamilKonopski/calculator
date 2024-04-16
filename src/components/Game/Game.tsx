3b1ab924009bed1d8953d3f82990b18df5758219
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2024-04-16 16:27:25 +0200

    add functions to increase stats

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,34 +1,42 @@
 import React, { useState, MouseEvent } from "react";
 
+import classes from "./Game.module.scss";
+
 interface IGameProps {
 	quizData: {
 		question: string;
 		options: { letter: string; option: string }[];
 		correct: string;
 	};
+	gameOver: () => void;
+	quizWin: () => void;
+	quizLose: () => void
 }
 
-const Game: React.FC<IGameProps> = ({ quizData }) => {
+const Game: React.FC<IGameProps> = ({ quizData, gameOver, quizWin, quizLose }) => {
 	const [isCorrect, setIsCorrect] = useState<boolean | null>(null);
 	const handleCorrectAnswer = (event: MouseEvent<HTMLButtonElement>) => {
 		if (event.currentTarget.innerText.slice(1).toLocaleLowerCase().trim() === quizData.correct.toLocaleLowerCase()) {
-			setIsCorrect(true);
+			quizWin();
+			gameOver();
 		} else {
 			setIsCorrect(false);
+			quizLose();
+			gameOver();
 		}
 	};
 
 	return (
-		<div>
-			<h2>{quizData.question}</h2>
-			<ul>
+		<div className={classes.game}>
+			<h2 className={classes["game__question"]}>{quizData.question}</h2>
+			<div className={classes["game__options"]}>
 				{quizData.options.map(({ letter, option }) => (
-					<button onClick={handleCorrectAnswer}>
-						<span>{letter} </span>
+					<button className={classes["game__option"]} key={letter} onClick={handleCorrectAnswer}>
+						<span className={classes["game__letter"]}>{letter} </span>
 						{option}
 					</button>
 				))}
-			</ul>
+			</div>
 		</div>
 	);
 };
